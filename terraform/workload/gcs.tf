24d46ae80566fb71e503b2b9101d8064965e1295
jjchange10 <jj.change10@icloud.com>
2025-11-17 15:57:47 +0900

    workflow test

--- terraform/workload/gcs.tf
+++ terraform/workload/gcs.tf
@@ -1,37 +1,37 @@
-# resource "google_storage_bucket" "main" {
-#   name          = "${var.base_project_id}-bucket"
-#   project       = var.base_project_id
-#   location      = var.region
-#   force_destroy = false
+resource "google_storage_bucket" "main" {
+  name          = "${var.base_project_id}-bucket"
+  project       = var.base_project_id
+  location      = var.region
+  force_destroy = false
 
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   lifecycle_rule {
-#     condition {
-#       age = 90
-#     }
-#     action {
-#       type = "Delete"
-#     }
-#   }
+  lifecycle_rule {
+    condition {
+      age = 90
+    }
+    action {
+      type = "Delete"
+    }
+  }
 
-#   lifecycle_rule {
-#     condition {
-#       age                   = 30
-#       with_state            = "ANY"
-#       num_newer_versions    = 3
-#     }
-#     action {
-#       type = "Delete"
-#     }
-#   }
+  lifecycle_rule {
+    condition {
+      age                   = 30
+      with_state            = "ANY"
+      num_newer_versions    = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# バケット用のIAMバインディング（必要に応じてコメント解除）
+# resource "google_storage_bucket_iam_member" "member" {
+#   bucket = google_storage_bucket.main.name
+#   role   = "roles/storage.objectViewer"
+#   member = "serviceAccount:${google_service_account.gke_workload_sa.email}"
 # }
-
-# # バケット用のIAMバインディング（必要に応じてコメント解除）
-# # resource "google_storage_bucket_iam_member" "member" {
-# #   bucket = google_storage_bucket.main.name
-# #   role   = "roles/storage.objectViewer"
-# #   member = "serviceAccount:${google_service_account.gke_workload_sa.email}"
-# # }
